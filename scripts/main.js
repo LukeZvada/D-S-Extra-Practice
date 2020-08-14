6a73c25f281929d2e5f3ff2e63c996d078916e62
LukeZvada <zvadamusic@gmail.com>
2020-08-14 09:57:29 -0700

    cleaned up main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
 import { businessList, NewYorkCompanies, manufacturingCompanies } from "./BusinessList.js";
-// import { business } from "./Business.js";
+
 
 
 
 businessList()
-// business()
 NewYorkCompanies()
 manufacturingCompanies()
