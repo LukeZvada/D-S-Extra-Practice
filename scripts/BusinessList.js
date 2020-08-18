0256893d709e812cf5a8b5f4e11b37245222400d
LukeZvada <zvadamusic@gmail.com>
2020-08-18 12:09:32 -0700

    eventListener created using .includes(). Made it searchable by lowerCase by .toLowerCase()

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -41,4 +41,41 @@ export const agent = () => {
     })
 }
 
-const businessSearchResult = document.querySelector(".foundCompanies")
+
+const companySearchResultArticle = document.querySelector(".foundCompanies")
+
+document
+    .querySelector("#companySearch")
+    .addEventListener("keypress", keyPressEvent => {
+        if (keyPressEvent.charCode === 13) {
+           const bizArray = useBusiness()
+           const searchValue = keyPressEvent.target.value.toLowerCase()
+           
+           return (firstName.includes(searchValue) || lastName.includes(searchValue))
+            /*
+                When user presses enter, find the matching business.
+                You can use the `.includes()` method strings to
+                see if a smaller string is part of a larger string.
+
+                Example:
+                    business.companyName.includes(keyPressEvent.target.value)
+            */
+
+            const foundBusiness = // implement .find() method here
+
+            companySearchResultArticle.innerHTML = `
+                <h2>
+                ${foundBusiness.companyName}
+                </h2>
+                <section>
+                ${foundBusiness.addressFullStreet}
+
+                </section>
+                <section>
+                ${foundBusiness.addressCity},
+                ${foundBusiness.addressStateCode}
+                ${foundBusiness.addressZipCode}
+                </section>
+            `;
+        }
+    });
